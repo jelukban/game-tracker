0c6127936caa6a42887499078da2c1ed12cce71c
Jerianne Lukban <jeriannelukban19@gmail.com>
2022-11-08 21:02:15 -0800

    Fixed issue on html where incorrect React syntax was used

--- static/js/app.jsx
+++ static/js/app.jsx
@@ -3,14 +3,19 @@
 function Homepage() {
     return (
         <div>
-            <h1>Sign into your account</h1>
-            <form id="login" method="POST">
-                <label for="email">Email Address</label>
-                <input type="text" id="email"></input>
-                <label for="password"> Password</label>
-                <input type="password"></input>
-                <button type="submit">Sign In</button>
-            </form>
+            <div> Login Form
+                <h1>Sign into your account</h1>
+                <form id="login" method="POST">
+                    <label htmlFor="email">Email Address</label>
+                    <input type="text" id="email"></input>
+                    <label htmlFor="password"> Password</label>
+                    <input type="password" id='password'></input>
+                    <button type="submit">Sign In</button>
+                </form>
+            </div>
+            <div>
+                <VideoGameContainer />
+            </div>
         </div>
     );
 };
--- static/js/components.jsx
+++ static/js/components.jsx
@@ -1,20 +1,43 @@
 'use strict';
 
-function VideoGame({name, description}) {
+function VideoGame({name, description, release_date}) {
     return (
-    <div class="video-game">
+    <div className="video-game">
+        <p>Video game cover goes here</p>
         <p> {name}</p>
         <p> {description} </p>
+        <p> {release_date}</p>
     </div>
     );
 };
 
+
 function VideoGameContainer() {
 
     const [games, setGames] = React.useState([]);
+
+    React.useEffect(() => {
+        fetch('/api/games')
+        .then((response) => response.json())
+        .then((responseJson) => setGames(responseJson.games))
+    }, []);
+
     const videoGames = [];
 
     for (const currentGame of games) {
-        videoGames.push(<VideoGame name={})
+        videoGames.push(
+            <VideoGame
+            name={currentGame.name}
+            decription={currentGame.description}
+            release_date={currentGame.release_date}
+            />
+        );
     };
-};
+
+    return (
+        <React.Fragment>
+            <h2> Video Games </h2>
+            <div className="grid">{videoGames}</div>
+        </React.Fragment>
+    );
+}
