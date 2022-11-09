dc6a700050b96146ae61c7552d9dd16536319ec7
Jerianne Lukban <jeriannelukban19@gmail.com>
2022-11-08 21:00:32 -0800

    Fixed error when trying to call get_all_games function

--- crud.py
+++ crud.py
@@ -3,5 +3,21 @@
 from model import db, User, Game, connect_to_db
 
 
-def create_game(name, description, game_image):
-    return Game(name=name, description=description, game_image=game_image)
+def create_game(name, description, game_image, release_date):
+    """ Creates a video game to add to the Game db. """
+
+    return Game(name=name,
+        description=description,
+        game_image=game_image,
+        release_date=release_date)
+
+
+def get_all_games():
+    """ Returns all video games in the Game db. """
+
+    return db.session.query(Game).all()
+
+if __name__ == '__main__':
+
+    from server import app
+    connect_to_db(app)
