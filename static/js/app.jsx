9037b271f9f7001adebda98a639b371f88269cca
Jerianne Lukban <jeriannelukban19@gmail.com>
2022-11-11 19:34:59 -0800

    Added App large component to render homepage and login page

--- static/js/app.jsx
+++ static/js/app.jsx
@@ -1,9 +1,18 @@
 'use strict';
 
-function Homepage() {
+function Homepage () {
     return (
         <div>
-            <div> Login Form
+            <div>
+                <VideoGameContainer />
+            </div>
+        </div>
+    );
+};
+
+function LoginPage () {
+    return (
+        <div> Login Form
                 <h1>Sign into your account</h1>
                 <form id="login" method="POST">
                     <label htmlFor="email">Email Address</label>
@@ -13,11 +22,44 @@ function Homepage() {
                     <button type="submit">Sign In</button>
                 </form>
             </div>
-            <div>
-                <VideoGameContainer />
-            </div>
+    );
+};
+
+function Navbar () {
+    pass;
+};
+
+function MainContent () {
+    const [currentPage, setCurrentPage] = React.useState('');
+
+    function renderContent () {
+        if (currentPage === 'homepage') {
+            return <Homepage></Homepage>
+        }
+        if (currentPage === 'login') {
+            return <LoginPage></LoginPage>
+        };
+    };
+
+    return (
+        <div>
+            <button onClick={() => setCurrentPage('homepage')}>Homepage</button>
+            <button onClick={() => setCurrentPage('login')}>Login</button>
+            {renderContent()}
+        </div>
+        );
+};
+
+function SideContent () {
+    pass;
+};
+
+function App() {
+    return (
+        <div>
+            <MainContent />
         </div>
     );
 };
 
-ReactDOM.render(<Homepage />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
