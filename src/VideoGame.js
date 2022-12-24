0761ac8406e15a2832897287102a7074dc847428
Jerianne Lukban <jeriannelukban19@gmail.com>
2022-12-23 16:43:42 -0800

    Added key for individual video games and a class name

--- src/VideoGame.js
+++ src/VideoGame.js
@@ -1,24 +1,15 @@
-import React from 'react';
 import { Link, BrowserRouter, Route, Navigate, Routes } from 'react-router-dom';
 
 function VideoGame({game_id, cover_url, name, release_date, genres, platforms}) {
 
     return (
-        <React.Fragment>
-                <Link to={`/games/details/${game_id}`} >
-                <div >
-                    <p>{name}</p>
-                    <img src={cover_url} height="128" width="90" ></img>
-                    <div>
-                        Genres: {genres?.map(genre => <div>{genre}</div>)}
-                    </div>
-                    <div>
-                        Platforms: {platforms?.map(platform => <div>{platform}</div>)}
-                    </div>
-                    <p> Release Date: {release_date}</p>
-                </div>
-                </Link>
-    </React.Fragment>
+        <Link to={`/games/details/${game_id}`} >
+            <p>{name}</p>
+            <img src={cover_url} height="128" width="90" ></img>
+                Genres: {genres?.map(genre => <div className="game-genre">{genre}</div>)}
+                Platforms: {platforms?.map(platform => <div className="game-platform">{platform}</div>)}
+            <p> Release Date: {release_date}</p>
+        </Link>
     );
 };
 
--- src/VideoGameContainer.js
+++ src/VideoGameContainer.js
@@ -4,12 +4,12 @@ import { useState } from 'react';
 function VideoGameContainer({games}) {
     return (
             <div className="grid">
-            {games.map(game => <VideoGame game_id={game.id}
-                                                        cover_url={game.cover_url}
-                                                        name={game.name}
-                                                        release_date={game.release_date}
-                                                        genres={game.genres.replaceAll(`'`, ``).replace(`\[`, '').replace(`\]`, '').split(',')}
-                                                        platforms={game.platforms.replaceAll(`'`, ``).replace(`\[`, '').replace(`\]`, '').split(',')}/>)}
+            {games.map(game => <div key={game.id} className="game"><VideoGame game_id={game.id}
+                                    cover_url={game.cover_url}
+                                    name={game.name}
+                                    release_date={game.release_date}
+                                    genres={game.genres.replaceAll(`'`, ``).replace(`\[`, '').replace(`\]`, '').split(',')}
+                                    platforms={game.platforms.replaceAll(`'`, ``).replace(`\[`, '').replace(`\]`, '').split(',')}/></div>)}
             </div>
     );
 };
