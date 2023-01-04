9a8d17173fb91e2ad7925f308818828804e7953e
Jerianne Lukban <jeriannelukban19@gmail.com>
2023-01-03 20:45:05 -0800

    Added bootstrap carousel to recommended video games

--- src/UserRecommendations.js
+++ src/UserRecommendations.js
@@ -1,8 +1,17 @@
 import { React, useEffect, useParams, useState } from 'react';
-import VideoGameContainer from './VideoGameContainer.js'
+import VideoGame from './VideoGame.js';
+import Carousel from 'react-bootstrap/Carousel';
+
 
 function UserRecommendations ({user}) {
     const [games, setGames] = useState([]);
+
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex, e) => {
+      setIndex(selectedIndex);
+    };
+
 
     useEffect(() => {
         fetch(`/api/dashboard/${user.id}`)
@@ -13,11 +22,19 @@ function UserRecommendations ({user}) {
     }, []);
 
     return (
-        <div>
-            <div className="user-recommendations">
-                Here are your recommended games!
-                <VideoGameContainer games={games} />
-            </div>
+        <div className="recommended-games">
+            <h1> Your recommendations </h1>
+            <Carousel activeIndex={index} onSelect={handleSelect} variant="dark">
+                {games.map(game =>  <Carousel.Item>
+                                        <VideoGame game_id={game.id}
+                                            cover_url={game.cover_url}
+                                            name={game.name}
+                                            release_date={game.release_date}
+                                            genres={game.genres.replaceAll(`'`, ``).replace(`\[`, '').replace(`\]`, '').split(',')}
+                                            platforms={game.platforms.replaceAll(`'`, ``).replace(`\[`, '').replace(`\]`, '').split(',')}/>
+                                    </Carousel.Item>
+                                    )}
+            </Carousel>
         </div>
     );
 };
