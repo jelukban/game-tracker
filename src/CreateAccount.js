57d58a4b13e5677e77b725f009ceb483f3866ab2
Jerianne Lukban <jeriannelukban19@gmail.com>
2023-01-06 16:55:16 -0800

    Centered login and account creation forms

--- src/CreateAccount.js
+++ src/CreateAccount.js
@@ -8,23 +8,23 @@ function CreateAccount ({handleSubmit,setFirstName, setLastName, setEmail, setPa
 
     return (
         <React.Fragment>
-            <Form onSubmit={handleSubmit}> <h1>Create an Account</h1>
-                <Form.Group className="w-25" controlId="formFirstName">
+            <Form onSubmit={handleSubmit} className="forms"> <h1>Create an Account</h1>
+                <Form.Group className="w-100" controlId="formFirstName">
                     <Form.Label className="field-labels">First Name</Form.Label>
                     <Form.Control type="text" placeholder="Enter first name" onChange={setFirstName} />
                 </Form.Group>
 
-                <Form.Group className="w-25" controlId="formLastName">
+                <Form.Group className="w-100" controlId="formLastName">
                     <Form.Label className="field-labels">Last Name</Form.Label>
                     <Form.Control type="text" placeholder="Enter last name" onChange={setLastName} />
                 </Form.Group>
 
-                <Form.Group className="w-25" controlId="formBasicEmail">
+                <Form.Group className="w-100" controlId="formBasicEmail">
                     <Form.Label className="field-labels">Email address</Form.Label>
                     <Form.Control type="email" placeholder="Enter email" onChange={setEmail} />
                 </Form.Group>
 
-                <Form.Group className="w-25" controlId="formBasicPassword">
+                <Form.Group className="w-100" controlId="formBasicPassword">
                     <Form.Label className="field-labels">Password</Form.Label>
                     <Form.Control type="password" placeholder="Password" onChange={setPassword} mb={8}/>
                     <Form.Text className="text-muted"> Password must be at least 8 characters
--- src/LoginPage.js
+++ src/LoginPage.js
@@ -7,13 +7,13 @@ function LoginPage ({handleSubmit, setEmail, setPassword, showError }) {
 
     return (
         <React.Fragment>
-            <Form onSubmit={handleSubmit}> <h1>Welcome back gamer!</h1>
-                <Form.Group className="w-25" controlId="formBasicEmail">
+            <Form onSubmit={handleSubmit} className="forms"> <h1>Welcome back gamer!</h1>
+                <Form.Group className="w-100" controlId="formBasicEmail">
                 <Form.Label className="field-labels">Email address</Form.Label>
                 <Form.Control type="email" placeholder="Enter email" onChange={setEmail} />
                 </Form.Group>
 
-                <Form.Group className="w-25" controlId="formBasicPassword">
+                <Form.Group className="w-100" controlId="formBasicPassword">
                 <Form.Label className="field-labels">Password</Form.Label>
                 <Form.Control type="password" placeholder="Password" onChange={setPassword} mb={8}/>
                 </Form.Group>
