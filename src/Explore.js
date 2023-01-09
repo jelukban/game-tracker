43425c014c1a8e22510b99684d0b6ab81b7cd260
Jerianne Lukban <jeriannelukban19@gmail.com>
2023-01-09 15:57:46 -0800

    Darkened font of homepage and changed Explore header

--- src/Explore.js
+++ src/Explore.js
@@ -2,7 +2,7 @@ import GamePage from './GamePage.js';
 
 function Explore ({games}) {
     return (<div id="homepage">
-                <h1> Welcome to your gaming library!</h1>
+                <h1> Find Your Next Adventure </h1>
                 <GamePage games={games} />
             </div>
     );
