b13ad9769b756438650466a6e510b6b47f62a5fc
Jerianne Lukban <jeriannelukban19@gmail.com>
2024-11-17 19:14:55 -0800

    Started to incorporate redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import App from "./App.js";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import ReactDOM from "react-dom/client";
+import App from "./App.js";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
-root.render(<App />);
+root.render(
+  <Provider>
+    <App />
+  </Provider>
+);
