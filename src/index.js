04d31fbbeab9ab19ea482d1d5bde34110d546fb0
Jerianne Lukban <jeriannelukban19@gmail.com>
2024-11-24 13:10:35 -0800

    Added redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { store } from "./redux/store.js";
 import ReactDOM from "react-dom/client";
 import App from "./App.js";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>
 );
